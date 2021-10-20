a427b5a6075ded4523555da20b0e82de27842e0a
GlazkovPavel <glazprom@mail.ru>
2021-10-20 22:49:45 +0300

    feat: Настроил express

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const mongoose = require("mongoose");
 
 
 const { PORT = 3001, BASE_PATH } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/movies-explorer');
+
 
 
 
